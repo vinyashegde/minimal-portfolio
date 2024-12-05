44f56164ab51db37d406a8cd9ecec67fff8362d5
Vinyas Hegde <vinyasvasanthegde7@gmail.com>
2024-12-05 09:31:43 +0000

    updated images

--- components/projects-grid.tsx
+++ components/projects-grid.tsx
@@ -11,7 +11,7 @@ const projects = [
   {
     title: "BackDrop - Text Behind Image",
     description: "a web tool for creating stunning text effects with text behind images",
-    image: "https://private-user-images.githubusercontent.com/46837876/384619377-e831e644-11ed-46a9-b608-33053472b722.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MzMzODg5ODEsIm5iZiI6MTczMzM4ODY4MSwicGF0aCI6Ii80NjgzNzg3Ni8zODQ2MTkzNzctZTgzMWU2NDQtMTFlZC00NmE5LWI2MDgtMzMwNTM0NzJiNzIyLnBuZz9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFWQ09EWUxTQTUzUFFLNFpBJTJGMjAyNDEyMDUlMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjQxMjA1VDA4NTEyMVomWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPWE0YTA2ZmVlNjdmNThiM2NkOGI4MjY2ZjYyYTQ4MDFmOTkyYWZlYTM4MjczMGViOGIwMmJiNTIxM2E5YjkyZWUmWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0In0.iIeEo4XyqUwF7b5aIEQRoeHzuUgzq6QXWIkTZYZiwAQ",
+    image: "https://github.com/vinyashegde/backdrop-text-behind-image/blob/main/384619377-e831e644-11ed-46a9-b608-33053472b722.png?raw=true",
     technologies: ["Python", "Flask", "Tailwind"],
     category: "Web",
     demoUrl: "https://backdrop-text-behind-image.vercel.app/",
@@ -37,7 +37,7 @@ const projects = [
   {
     title: "My App",
     description: "A Flutter app blending To-Do, Kanban, and Lo-Fi",
-    image: "https://private-user-images.githubusercontent.com/46837876/382555716-f609eb9e-6977-4cc4-9858-670c29922874.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MzMzODkzMTcsIm5iZiI6MTczMzM4OTAxNywicGF0aCI6Ii80NjgzNzg3Ni8zODI1NTU3MTYtZjYwOWViOWUtNjk3Ny00Y2M0LTk4NTgtNjcwYzI5OTIyODc0LnBuZz9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFWQ09EWUxTQTUzUFFLNFpBJTJGMjAyNDEyMDUlMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjQxMjA1VDA4NTY1N1omWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPTEzOTc3YmUxNGY3ZmM5Y2FiNWY4ZjdmYTUzMjQ1NWNiZGFmNjE1Y2JjNjc4NjcwYWUzNmI5NWMxYzU3Y2VjNmQmWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0In0.UJGi2SdWz5NOPSJAh7skW_I4eMRFn5EcazKXxWjs2j0",
+    image: "https://github.com/vinyashegde/myapp/blob/main/382555716-f609eb9e-6977-4cc4-9858-670c29922874.png?raw=true",
     technologies: ["Flutter", "Firebase"],
     category: "Android",
     sourceUrl: "https://github.com/vinyashegde/myapp",
